d40c5c27399bdfbaed005e0488337251c3802c62
olivier <olivier@evaletolab.ch>
2022-02-08 11:52:55 +0100

    chg: update content

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,7 @@
       <li>BIP32 Root Key</li>
       <li>SSS (Shamir Shared Secret)</li>
       <li>Vault Solidity Smart Contract</li>
-      <li>Licence code is open source</li>
+      <li>Licence code is available on <a href="https://github.com/evaletolab/horcrux-wallets">github</a></li>
       <li>Authors </li>
     </ul>
   </div>
