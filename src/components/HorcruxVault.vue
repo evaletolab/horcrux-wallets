33adee6f0895e3043b89368dbf8df1aefd6f817d
olivier <olivier@evaletolab.ch>
2022-02-16 14:06:16 +0100

    new: use of password strenght

--- src/components/HorcruxVault.vue
+++ src/components/HorcruxVault.vue
@@ -11,10 +11,11 @@
 
     <form>
       <fieldset>
-        <label for="email">email</label>
+        <label for="email">Use an email to generate your secret</label>
         <input type="email"  v-model="username" placeholder="email@g.com" id="email" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false">
-        <label for="pwd">Secret password (min 8 chars)</label>
-        <input type="password"  v-model="password" id="pwd"  placeholder="***" _pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$" autocomplete="new-password" autocorrect="off" autocapitalize="off" spellcheck="false">
+        <password label="Complete your secret with a hard password (min 8 chars)" 
+                  v-model="password" />
+
         <button @click="onPublish" class="button-primary" >Generate Vault </button>
         <button class="button-primary" >Publish </button>
       </fieldset>
@@ -58,11 +59,14 @@
 <script lang="ts">
 import { requiresWork } from '@/lib/POW';
 import { stringToHEX256 } from '@/lib/utils';
-import { BigNumber, ethers } from 'ethers';
+import { BigNumber, ethers, Signer } from 'ethers';
 import { Options, Vue } from 'vue-class-component';
 import { $wallet, Horcrux } from '../services';
 
+import Password from '@/components/Password.vue';
+
 @Options({
+  components: { Password },
   props: {
     value: {
       default: {},
@@ -81,20 +85,96 @@ export default class HorcruxVault extends Vue {
   username = "";
   difficulty = BigNumber.from('0x1ffff');
 
+  // Web3
+  signer: Signer|null=null;
+  account = "";
+  balance = "0";
+
 
   get date() {
     return this.currentDate.toLocaleString();
   }
 
-  onPublish($event:Event) {
-    const seed = stringToHEX256(this.username+""+this.password);
-    const nonce = '0x'+requiresWork(seed,this.difficulty)[1];
-    console.log('----- POW',seed,nonce);
-    $event.preventDefault();
-    // const node = ethers.utils.HDNode.fromSeed(seed);
-    // const hash =  ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['uint256','uint256'],[seed,nonce]));
-    //horcrux.create(hash,this.value.base64);
 
+  async initMetamask() {
+    const $window = window as any;
+    if(!$window.ethereum){
+      throw new Error('Please install MetaMask');
+    }
+    //const web3 = new Web3($window.ethereum);
+    // const netId = await web3.eth.net.getId();
+    // const accounts = await web3.eth.getAccounts();    
+    // const metamask = await $window.ethereum.request({ method: 'eth_requestAccounts' });
+    // const account = accounts[0] || metamask[0];
+    //
+    // initialize metamask
+    const [account] = await $window.ethereum.request({ method: 'eth_requestAccounts' });
+    if(!account){
+      throw new Error('Please login with MetaMask');
+    }
+    const provider = new ethers.providers.Web3Provider($window.ethereum);
+    this.signer = provider.getSigner();
+    // console.log('--- DBG signer0',await this.signer.getAddress());
+    // console.log('--- DBG signer1',await this.signer.getChainId());
+    // console.log('--- DBG signer2',(await this.signer.getBalance()).toString());
+
+
+    // To connect to the user's wallet, we have to run this method.
+    // It returns a promise that will resolve to the user's address.
+    this.account = account;
+
+    //
+    //load balance
+    // this.balance = await web3.eth.getBalance(account);
+    this.balance = (await provider.getBalance(this.account)).toString();
+  }
+
+    //
+    //load contracts
+  //   try {
+  //     this.token = new ethers.Contract(
+  //       this.Token.address,
+  //       this.Token.abi,
+  //       signer
+  //     );
+  //     this.dbank = new ethers.Contract(
+  //       this.dBank.address,
+  //       this.dBank.abi,
+  //       signer
+  //     );
+  //     // this.token.on('MinterChanged',(from: any, to: any) =>{
+  //     //   console.log('--- DBG MinterChanged evnt',from,to);
+  //     // });
+  //     // this.dbank.on('Deposit',(user: any, etherAmount: any, timeStart: any) =>{
+  //     //   console.log('--- DBG deposit evnt',user,etherAmount.toString());
+  //     //   this.depositedAmount = (etherAmount.toString());
+  //   } catch (e) {
+  //     console.log('Error', e);
+  //     window.alert('Contracts not deployed to the current network')
+  //   }
+  // }
+
+  async onPublish($event:Event) {
+    $event.preventDefault();
+    try{
+      await this.initMetamask();
+
+      //
+      // proof of Work
+      const seed = stringToHEX256(this.username+""+this.password);
+      const nonce = '0x'+requiresWork(seed,this.difficulty)[1];
+
+      console.log('----- POW is done: ',seed,nonce);
+      window.alert('POW is done,' + seed);
+
+      // const node = ethers.utils.HDNode.fromSeed(seed);
+      // const hash =  ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['uint256','uint256'],[seed,nonce]));
+      //horcrux.create(hash,this.value.base64);
+
+    }catch(err: any) {
+      console.debug(err);
+      window.alert(err.message);
+    }
   }
 
 }
---
+++ src/components/Password.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <div>
+      <label>{{ label }}</label>
+      <input type="password"  @input="password = $event.target.value"
+        _pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$" autocomplete="new-password" autocorrect="off" autocapitalize="off" spellcheck="false"/>
+      <div class="strenght">
+        <span class="pad"
+          :class="{
+            red: score <= 1,
+            orange: score == 2,
+            yellow: score == 3,
+            green: score == 4,
+          }"
+        ></span>
+        <span class="pad"
+          :class="{
+            gray: score <= 1,
+            orange: score == 2,
+            yellow: score == 3,
+            green: score == 4,
+          }"
+        ></span>
+        <span class="pad"
+          :class="{
+            gray: score <= 2,
+            yellow: score == 3,
+            green: score == 4,
+          }"
+        ></span>
+        <span class="pad"
+          :class="{
+            gray: score <= 3,
+            green: score == 4,
+          }"
+        ></span>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  input{
+    margin-bottom: 5px!important;
+  }
+
+  .strenght{
+    margin-bottom: 15px!important;
+    display: flex;
+    justify-content: space-between;
+    .pad{
+      width: 24%;
+      height: 5px;
+      border-radius: 5px;
+      display: block;    
+    }
+    .red{
+      background: red;
+    }
+    .orange{
+      background: orange;
+    }
+    .yellow{
+      background: yellow;
+    }
+    .green{
+      background: green;
+    }
+
+    .gray{
+      background: #eee;
+    }
+
+  }
+</style>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+import { $wallet, Horcrux } from "../services";
+import zxcvbn from 'zxcvbn-typescript';
+
+//
+// two ways binding 
+// https://www.digitalocean.com/community/tutorials/how-to-add-v-model-support-to-custom-vue-js-components
+@Options({
+  props: {
+    label: {
+      default: "",
+      required: true,
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+  },
+})
+export default class Password extends Vue {
+  score = 0;
+  label!: string;
+  value!: string;
+
+  get password() {
+    return this.value;
+  }
+  set password(value) {
+    this.score = zxcvbn(value||'').score;
+    console.log('-----emit',value, this.score)
+    this.$emit('input', value);   
+  }
+}
+</script>
+
