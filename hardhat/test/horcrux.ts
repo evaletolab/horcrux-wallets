dd6332891b9104f3aff067520d30ee33b25ceec6
olivier <olivier@evaletolab.ch>
2022-02-24 16:57:18 +0100

    new: implement mixer

--- hardhat/test/horcrux.ts
+++ hardhat/test/horcrux.ts
@@ -3,6 +3,7 @@ import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { requiresWork, proofOfWork } from "../../src/lib/POW";
 import { stringToHEX256 } from "../../src/lib/utils";
+import { xor_encrypt, xor_decrypt } from "../../src/lib/XOR";
 describe("Horcrux", function () {
 
   const difficulty = BigNumber.from('0xafff');
@@ -23,6 +24,30 @@ describe("Horcrux", function () {
     expect(pow).to.be.true;
   })
 
+  it("mix and unmix (XOR) buffer data", async function () {
+    const pkey ="0xc2cdaf9afe1289a5545326e2";
+    const share = "0x0801fde48137222a357d4a70db809ae103aae7864b2312ab448a9301df635ea8020b";
+
+    //
+    // simple mixer
+    let bytes = xor_encrypt(
+      ethers.utils.arrayify(share),
+      ethers.utils.arrayify(pkey)
+    );
+    const mixed = ethers.utils.hexlify(bytes);
+
+    //
+    // simple demixer
+    bytes = xor_decrypt(
+      ethers.utils.arrayify(mixed),
+      ethers.utils.arrayify(pkey)
+    );
+    const unmixed = ethers.utils.hexlify(bytes);
+
+    expect(share).to.equal(unmixed);
+
+  })
+
   it("create a new horcrux", async function () {
     const Horcrux = await ethers.getContractFactory("Horcrux");
     const horcrux = await Horcrux.deploy();
@@ -31,15 +56,33 @@ describe("Horcrux", function () {
 
     const seed = stringToHEX256("userfrom1233@bigcomp.com1LP#QOlp09");
     const node = ethers.utils.HDNode.fromSeed(seed);
-    const share = "0x801fde48137222a357d4a70db809ae103aae7864b2312ab448a9301df635ea8020b";
-    const shareB64 = "gB/eSBNyIqNX1KcNuAmuEDqueGSyMSq0SKkwHfY16oAgsA=="; // len 48
+    const share = "0x0801fde48137222a357d4a70db809ae103aae7864b2312ab448a9301df635ea8020b";
     const nonce = '0x'+requiresWork(seed,difficulty)[1];
     const hash =  ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['uint256','uint256'],[seed,nonce]));
 
-    await horcrux.create(hash,share);
+    
+    //
+    // simple mixer
+    let bytes = xor_encrypt(
+      ethers.utils.arrayify(share),
+      ethers.utils.arrayify(node.privateKey.substring(0,16))
+    );
+    const mixed = ethers.utils.hexlify(bytes);
+    
+
+    await horcrux.create(hash,mixed);
     const result = await horcrux.redeem(seed,nonce);
-    //console.log('---share ',result);
-    expect(result).to.equal(share);
+
+    //
+    // simple demixer
+    bytes = xor_decrypt(
+      ethers.utils.arrayify(result),
+      ethers.utils.arrayify(node.privateKey.substring(0,16))
+    );
+    const unmixed = ethers.utils.hexlify(bytes);
+
+
+    expect(share).to.equal(unmixed);
   });
   
 
@@ -49,7 +92,6 @@ describe("Horcrux", function () {
 
     const iface = new ethers.utils.Interface(abiCreate);
     const share = "0x801fde48137222a357d4a70db809ae103aae7864b2312ab448a9301df635ea8020b";
-    const shareB64 = "gB/eSBNyIqNX1KcNuAmuEDqueGSyMSq0SKkwHfY16oAgsA=="
     const nonce = '0x'+requiresWork(seed,difficulty)[1];
     const encoder = ethers.utils.defaultAbiCoder.encode(['uint256','uint256'],[seed,nonce])
     const source = ethers.utils.keccak256(encoder);
