270b8040bc4c6621ead56482da3947b785f3f250
olivier <olivier@evaletolab.ch>
2022-03-11 09:45:40 +0100

    chg: update text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="about  container">
-    <h1>Horcruxs are made with</h1>
+    <h1>Horcrux is an educational project made with <span class="pink">❤️</span> and based on the following works</h1>
     <ul>
+      <li><a href="https://iancoleman.io/bip39" >the great Ian Coleman bip39 demonstration</a></li>
       <li>Entropy</li>
       <li><a href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki">BIP39</a> Mnemonic</li>
       <li><a href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki">BIP39</a> Seed</li>
       <li><a href="https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki">BIP32</a> Root Key</li>
       <li><a href="https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki">BIP44</a> derivation path</li>
+      <li><a href="https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki">BIP84</a> Segwit derivation path</li>
       <li>SSS (Shamir Shared Secret)</li>
-      <li>Vault Solidity Smart Contract</li>
-      <li>Source code with MIT on <a href="https://github.com/evaletolab/horcrux-wallets">github</a></li>
+      <li><a href="https://github.com/evaletolab/horcrux-wallets/blob/oe-hardhat/hardhat/contracts/Horcrux.sol">HorcruxVault Smart Contract</a></li>
+      <li>Shared with MIT licence <a href="https://github.com/evaletolab/horcrux-wallets">github</a></li>
     </ul>
     <package-versions />
   </div>
