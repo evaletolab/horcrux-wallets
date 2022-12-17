85351fe073ba645e60f77888c5aa8e0f5168c32a
olivier <olivier@evaletolab.ch>
2022-12-17 18:34:40 +0100

    new: deploy from any branch

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,9 +3,10 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
+    const branch = (await execa('git', ['rev-parse', '--abbrev-ref', 'HEAD'])).stdout;
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
-    console.log("Building started...");
+    console.log("Building started from ",branch, "... ");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
@@ -14,7 +15,7 @@ const fs = require("fs");
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["checkout", "-f", branch.trim()]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
